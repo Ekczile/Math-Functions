6743603e137d5a78f4da08ce7ae6a44a6f2d7e55
Steven <ekczile@gmail.com>
2018-09-24 14:37:30 -0400

    updated multi test

--- multi test.rb
+++ multi test.rb
@@ -9,22 +9,22 @@ class Add_test < Minitest::Test
         assert_equal(15, multiplication(5, 3))
     end
     def test_3
-        assert_equal(12, multiplication(10, 2))
+        assert_equal(20, multiplication(10, 2))
     end
     def test_4
-        assert_equal(15, multiplication(33, 16))
+        assert_equal(48, multiplication(3, 16))
     end
     def test_5
-        assert_equal(53, multiplication(25, 18))
+        assert_equal(450, multiplication(25, 18))
     end
     def test_6
-        assert_equal(88, multiplication(61, 19))
+        assert_equal(1159, multiplication(61, 19))
     end
     def test_7
-        assert_equal(94, multiplication(45, 16))
+        assert_equal(720, multiplication(45, 16))
     end
     def test_8
-        assert_equal(72, multiplication(41, 90))
+        assert_equal(3690, multiplication(41, 90))
     end
     def test_9
         assert_equal(7, multiplication(10, 61))
